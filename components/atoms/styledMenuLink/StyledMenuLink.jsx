c54ea8579f28ad3b75c2580ff94d604534141a2a
Alexander Löfqvist <alexander.lofqvist@gmail.com>
2020-08-26 20:23:50 +0200

    Initial push of atom StyledMenuLink

---
+++ components/atoms/styledMenuLink/StyledMenuLink.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom'
+
+
+export const StyledMenuLink = styled(NavLink)`
+  display: flex;
+  text-decoration: none;
+  color: ${({ theme }) => theme.main};
+  padding: 12px;
+  &:hover {
+    color: ${({ theme }) => theme.main};
+    background: ${({ theme }) => theme.main};
+  }
+  &:focus {
+    text-decoration: none;
+  }
+  &.link-active {
+    color: ${({ theme }) => theme.main};
+  }
+`;
