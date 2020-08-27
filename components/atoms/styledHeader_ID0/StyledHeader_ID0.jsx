7096148731c4b8e9a5f4e181e9039f66b9d77322
Alexander Löfqvist <alexander.lofqvist@gmail.com>
2020-08-27 18:34:54 +0200

    Update of atom
    Updated conditional check in atom StyledHeader_ID0

--- components/atoms/styledHeader_ID0/StyledHeader_ID0.jsx
+++ components/atoms/styledHeader_ID0/StyledHeader_ID0.jsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const StyledHeader_ID0 = styled.header`
     grid-area: h;
-    background-image: ${({background}) => background ? 'url("' + background + '")' : 'url()'};
+    background-image: ${({background}) => 'url("' + background + '")'};
     max-height: ${({maxHeight}) => maxHeight ? maxHeight : "500px"};
 `;
