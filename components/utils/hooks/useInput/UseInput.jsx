9d87e799342d5730b46b02eb45b38669cb12a76e
Cristina Löfqvist <57663680+stina-Lo@users.noreply.github.com>
2020-09-28 18:39:35 +0200

    Update UseInput.jsx
    Klantar mig....

--- components/utils/hooks/useInput/UseInput.jsx
+++ components/utils/hooks/useInput/UseInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useInput = initialValue => {
+export const useInput = initialValue => {
     const [value, setValue] = useState(initialValue)
 
     return {
