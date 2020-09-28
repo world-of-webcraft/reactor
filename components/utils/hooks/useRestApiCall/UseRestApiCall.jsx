7d432c91f129aefd9042303df3b7303d63cb0814
Cristina Löfqvist <57663680+stina-Lo@users.noreply.github.com>
2020-09-28 18:00:37 +0200

    Update UseRestApiCall.jsx

--- components/utils/hooks/useRestApiCall/UseRestApiCall.jsx
+++ components/utils/hooks/useRestApiCall/UseRestApiCall.jsx
@@ -1,22 +1,29 @@
 import { useState, useEffect } from "react";
 
 export const useRestApiCall = (apiURL) => {
-  const [hasError, setErrors] = useState(false);
-  const [data, setData] = useState({});
+    const [url] = useState(apiURL)
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState([]);
 
-  const fetchData = async (apiURL) => {
-    const res = await fetch(apiURL);
-    res
-      .json()
-      .then(res => setData(res))
-      .catch(err => setErrors(err));
-  }
+    useEffect(() => {
+        if (!url) return
+        fetch(url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setData(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
 
-  useEffect(() => {
-    fetchData();
-  });
-  if (hasError) {
-    hasError;
-  }
-  return data;
+            )
+    }, [url]);
+    return { data: data, isLoaded: isLoaded, error: error };
 }
