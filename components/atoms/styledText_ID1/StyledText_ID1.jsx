7b081fb0b1ad9f267d255a6a4732bba2ff770a45
Alexander Löfqvist <alexander.lofqvist@gmail.com>
2021-09-25 12:56:37 +0200

    Add of atom paragraph with solid border and mild radius

---
+++ components/atoms/styledText_ID1/StyledText_ID1.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+/**
+  * A paragraph with border solid, a mild radius and
+  * themed background color if provided.
+  * default values:
+  * border-radius: 25px
+  * props:
+  * borderRadius: border-radius
+  */
+export const StyledText_ID0 = styled.p`
+    background-color: ${({ theme }) => {
+    if (theme) {
+      if (theme.primaryLight) {
+        theme.primaryLight
+      } else if (theme.primaryDark) {
+        theme.primaryDark
+      }
+    }
+  }};
+  border-style: solid;
+  border-radius: ${({borderRadius}) => borderRadius ? borderRadius : "25px"};
+`
---
+++ components/atoms/styledText_ID1/index.js
@@ -0,0 +1,1 @@
+export { StyledText_ID1 } from './StyledText_ID1';
